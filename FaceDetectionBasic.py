6e521d95c7663eb387bb8f22aaa4c3f6f7794500
WangYao Laptop Brivisus <wangyaoneo@gmail.com>
2022-06-13 22:52:35 +0800

    face

--- FaceDetectionBasic.py
+++ FaceDetectionBasic.py
@@ -2,12 +2,12 @@ import cv2 as cv
 import mediapipe as mp
 import time
 
-cap = cv.VideoCapture("")
+cap = cv.VideoCapture("video.mp4")
 pTime = 0
 
 mpFaceDetection = mp.solutions.face_detection
 mpDraw = mp.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection()
+faceDetection = mpFaceDetection.FaceDetection(0.75)
 
 while True:
     success, img = cap.read()
@@ -15,10 +15,19 @@ while True:
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     results = faceDetection.process(imgRGB)
 
-    if results.dectections:
+    if results.detections:
         for id, detection in enumerate(results.detections):
-            print(id, detection)
-            mpDraw.draw_detection(img)
+            # print(id, detection)
+            # print(detection.score)
+            # print(detection.location_data.relative_bouding_box)
+            # mpDraw.draw_detection(img, detection)
+            print(detection.location_data.relative_bounding_box.xmin)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+
+            cv.rectangle(img, bbox, (0, 255, 0), 2)
 
     cTime = time.time()
     fps = 1 / (cTime-pTime)
@@ -28,4 +37,4 @@ while True:
 
 
     cv.imshow("face", img)
-    cv.waitKey(10)
+    cv.waitKey(1)
