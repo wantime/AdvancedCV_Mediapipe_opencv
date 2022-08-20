67918642de576a436955200946f4071efbcb3da9
WangYao Laptop Brivisus <wangyaoneo@gmail.com>
2022-08-20 23:01:35 +0800

    face detection module

--- FaceDetectionModule.py
+++ FaceDetectionModule.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import time
 
 
-class faceDetection():
+class faceDetector():
     def __init__(self, minDetectionConf=0.5, model=1):
         self.minDetectionConf = minDetectionConf
         self.model = model
@@ -12,22 +12,34 @@ class faceDetection():
         self.mpDraw = mp.solutions.drawing_utils
         self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionConf, self.model)
 
-    def findFace(self, img, draw=True):
+    def findFaces(self, img, draw=True):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.faceDetection.process(imgRGB)
 
+        bboxs = []
         if self.results.detections:
-            if draw:
-                for id, detection in enumerate(results.detection):
-                    self.mpDraw.draw_detection(img, detection)
-        return img
+            for id, detection in enumerate(self.results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                       int(bboxC.width * iw), int(bboxC.height * ih)
+                bboxs.append([id, bbox, detection.score])
+
+                if draw:
+                    img = self.fancyDraw(img, bbox,)
+                    #cv.rectangle(img, bbox, (255, 0, 255), 2)
+                    cv.putText(img, f'{int(detection.score[0] * 100)}%',
+                               (bbox[0], bbox[1] - 20), cv.FONT_HERSHEY_PLAIN,
+                               2, (255, 0, 255), 2)
+
+        return img, bboxs
 
     def findPosition(self, img, draw=True):
 
         bboxList = []
 
         if self.results:
-            for id, detection in enumerate(results.detection):
+            for id, detection in enumerate(self.results.detection):
                 bboxC = detection.location_data.relative_bounding_box
                 ih, iw, ic = img.shape
                 bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
@@ -37,44 +49,33 @@ class faceDetection():
                     cv.rectangle(img, bbox, (0, 255, 0), 2)
         return bboxList
 
+    def fancyDraw(self, img, bbox, l=30, t=5):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
 
-cap = cv.VideoCapture("video.mp4")
-pTime = 0
-
-mpFaceDetection = mp.solutions.face_detection
-mpDraw = mp.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection(0.75)
-
-while True:
-    success, img = cap.read()
-
-    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    results = faceDetection.process(imgRGB)
-
-    if results.detections:
-        for id, detection in enumerate(results.detections):
-            # print(id, detection)
-            # print(detection.score)
-            # print(detection.location_data.relative_bouding_box)
-            # mpDraw.draw_detection(img, detection)
-            print(detection.location_data.relative_bounding_box.xmin)
-            bboxC = detection.location_data.relative_bounding_box
-            ih, iw, ic = img.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
-                   int(bboxC.width * iw), int(bboxC.height * ih)
-
-            cv.rectangle(img, bbox, (0, 255, 0), 2)
-            cv.putText(img, f'{int(detection.score[0])}', (bbox[0], bbox[1] - 20),
-                       cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-
+        cv.rectangle(img, bbox, (255, 0, 255), 2)
+        # Top Left x,y
+        cv.line(img, (x,y), (x+l, y), (255, 0, 255), t)
+        cv.line(img, (x,y), (x, y+l), (255, 0, 255), t)
+        # Top Right x1,y
+        cv.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv.line(img, (x1, y), (x1, y + l), (255, 0, 255), t)
+        # Bottom Right x1,y1
+        cv.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+        # Bottom left x,y1
+        cv.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+        return img
 
 def main():
-    cap = cv.VideoCapture()
+    cap = cv.VideoCapture('video.mp4')
     pTime = 0
-
+    detector = faceDetector(0.4)
     while True:
         success, img = cap.read()
-
+        img, bboxs = detector.findFaces(img)
+        print(bboxs)
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
